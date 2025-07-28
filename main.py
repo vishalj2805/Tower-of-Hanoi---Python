3acc86e6c4c779b66c0642f6a57a83d7a877ac79
vishalj2805 <vishalj2805@gmail.com>
2025-07-28 23:30:30 +0530

    Added Starting Position function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 
 
 def console_presentation(positions: dict[str, list[str]]):
@@ -10,10 +10,11 @@ def console_presentation(positions: dict[str, list[str]]):
              "disk_6":"####6###",
              "disk_7":"####7####",
              "disk_8":"#####8####",
-             "disk_9":"#####9#####"}
+             "disk_9":"#####9#####"
+             }
 
     towers = list(positions.keys())
-    print(len(disks.get(positions.get(towers[0])[0])))
+
     for row in range(9,0,-1):
         if len(positions.get(towers[0])) < row and len(positions.get(towers[1])) < row and len(positions.get(towers[2])) < row:
             print("")
@@ -30,6 +31,18 @@ def console_presentation(positions: dict[str, list[str]]):
 
             print("")
 
+def starting_position(number_of_disks: int):
+    towers = {"tower_1":[], "tower_2":[], "tower_3":[]}
+    disks = [num for num in range(1, number_of_disks+1)]
+    for i in range(1, number_of_disks+1):
+        disk_no = random.choice(disks)
+        tower_no = random.choice(["tower_1", "tower_2", "tower_3"])
+        towers[tower_no].append(f"disk_{disk_no}")
+        disks.remove(disk_no)
+
+    return towers
+
+
 
 
 def hanoi_game(number_of_disks):
@@ -39,9 +52,10 @@ def hanoi_game(number_of_disks):
 
 # disks = input("Enter Number of disks: ")
 
-positioning = {"tower_1":["disk_6", "disk_7", "disk_5", "disk_2", "disk_1"],
-               "tower_2":["disk_3"],
-               "tower_3":["disk_4", "disk_8", "disk_9"]
-               }
+# positioning = {"tower_1":["disk_6", "disk_7", "disk_5", "disk_2", "disk_1"],
+#                "tower_2":["disk_3"],
+#                "tower_3":["disk_4", "disk_8", "disk_9"]
+#                }
+#
+console_presentation(starting_position(9))
 
-console_presentation(positioning)
